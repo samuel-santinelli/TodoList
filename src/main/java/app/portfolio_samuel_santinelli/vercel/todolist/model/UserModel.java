a9c3b54ac115d5cc83e731f0754cee19eeb58771
samuel-santinelli <samuelsantinelli@gmail.com>
2024-06-20 13:53:20 -0300

    feature: Creating the controller

---
+++ src/main/java/app/portfolio_samuel_santinelli/vercel/todolist/model/UserModel.java
@@ -0,0 +1,10 @@
+package app.portfolio_samuel_santinelli.vercel.todolist.model;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+}
